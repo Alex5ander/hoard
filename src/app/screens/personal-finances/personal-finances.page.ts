62325e37b49478ab45bfc4ac1b3e80ceecde1f05
Alexsander Gutierrez Gonçalves <alexsandergutierreez@gmail.com>
2022-04-13 13:13:18 +0000

    Ajustes

--- src/app/screens/personal-finances/personal-finances.page.ts
+++ src/app/screens/personal-finances/personal-finances.page.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonalFinancesPage implements OnInit {
 
+  items = [
+    {
+      title:'Como identificar seu padrão de vida',
+      subtitle: 'Conheça os erros mais comuns, ao gerenciar o dinheiro, e como podem ser evitados'
+    },
+    {
+      title:'Erro de finanças',
+      subtitle: 'Conheça os erros mais comuns, ao gerenciar o dinheiro, e como podem ser evitados'
+    },
+    {
+      title:'Organização financeira',
+      subtitle: 'Como se organizar de forma simples para reduzir problemas financeiros'
+    },
+  ];
+  
   constructor() { }
 
   ngOnInit() {
