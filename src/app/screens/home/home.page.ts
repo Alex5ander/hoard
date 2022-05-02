758ff5fb3a3f20432e836ff7fbd8a116ad1e6b4b
Alexsander Gutierrez <alexsandergutierreez@gmail.com>
2022-05-01 22:55:28 -0300

    correção

--- src/app/screens/home/home.page.ts
+++ src/app/screens/home/home.page.ts
@@ -10,11 +10,11 @@ import { IonRouterOutlet, Platform } from '@ionic/angular';
 export class HomePage {
 
   constructor(private platform: Platform, private ionRouterOutlet: IonRouterOutlet) {
-    this.platform.backButton.subscribeWithPriority(-1, () => {
-      if(!this.ionRouterOutlet.canGoBack()) {
-        App.exitApp();
-      }
-    });
+    // this.platform.backButton.subscribeWithPriority(-1, () => {
+    //   if(!this.ionRouterOutlet.canGoBack()) {
+    //     App.exitApp();
+    //   }
+    // });
   }
 
 }
